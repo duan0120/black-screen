133bdad34b3bc5abc4b2d8b7e88cdce98bf8d0e0
duanhongjin <duanhjlt@gmail.com>
2025-03-21 09:02:03 +0800

    chore: update description

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Black Screen",
-  description: "Enter Blackscreennow.space and feel the ultimate charm of black screen.",
+  description: "Enter Blackscreennow.space and experience the ultimate charm of black screen.",
 };
 
 export default function RootLayout({
