de387ec976bbc798fb362572e5f54e0cbac75eba
duanhongjin <duanhjlt@gmail.com>
2025-08-23 23:22:06 +0800

    update featured on

--- src/app/components/ui/FeaturedOn.js
+++ src/app/components/ui/FeaturedOn.js
@@ -4,7 +4,9 @@ export default function FeaturedOn() {
     <div className='flex items-center flex-wrap gap-1'>
       <Link href="https://aistage.net" className="text-sm hover:text-primary" target="_blank">AIStage</Link>
       <span className="mx-2">|</span>
-      <Link href="https://startupfa.me/s/brat-gen?utm_source=brat-gen.com" target="_blank"><img src="https://startupfa.me/badges/featured/default.webp" alt="Featured on Startup Fame" className='h-[36px] w-auto' /></Link>
+      <Link href="https://startupfa.me/s/black-screen?utm_source=black-screen.cc" target="_blank">
+        <img src="https://startupfa.me/badges/featured-badge-small.webp" alt="Featured on Startup Fame" className='h-[36px] w-auto' />
+      </Link>
     </div>
   )
 }
