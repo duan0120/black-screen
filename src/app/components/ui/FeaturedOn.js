3065b56ebe34ec5a556a6100e07a6908dc9d3061
duanhongjin <duanhjlt@gmail.com>
2025-08-26 22:49:31 +0800

    update friend link

--- src/app/components/ui/FeaturedOn.js
+++ src/app/components/ui/FeaturedOn.js
@@ -7,6 +7,10 @@ export default function FeaturedOn() {
       <Link href="https://startupfa.me/s/black-screen?utm_source=black-screen.cc" target="_blank">
         <img src="https://startupfa.me/badges/featured-badge-small.webp" alt="Featured on Startup Fame" className='h-[36px] w-auto' />
       </Link>
+      <span className="mx-2">|</span>
+      <a href="https://turbo0.com/item/black-screen" target="_blank" rel="noopener noreferrer">
+        <img src="https://img.turbo0.com/badge-listed-light.svg" alt="Listed on Turbo0" className='h-[36px] w-auto' />
+      </a>
     </div>
   )
 }
